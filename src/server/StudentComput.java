383a501d03c396415b261667cf639bb2678a03f9
Marc <marc-saba@hotmail.com>
2021-04-28 13:58:52 +0200

    Substituted user class for StudentComput class better name and modified methods

---
+++ src/server/StudentComput.java
@@ -0,0 +1,36 @@
+package server;
+
+
+public class StudentComput {
+
+    private int correctAnswers;
+    private int currentQuestion;
+
+    public StudentComput(int correctAnswers, int currentQuestion) {
+
+        this.correctAnswers = correctAnswers;
+        this.currentQuestion = currentQuestion;
+    }
+
+    public StudentComput() {
+        this.correctAnswers = 0;
+        this.currentQuestion = 0;
+    }
+
+    public void nextQuestionCorrect() {
+        this.correctAnswers += 1;
+        this.currentQuestion += 1;
+    }
+
+    public void nextQuestion() {
+        this.currentQuestion += 1;
+    }
+
+    public Integer getCurrentQuestion() {
+        return currentQuestion;
+    }
+
+    public Integer getCorrectAnswers() {
+        return correctAnswers;
+    }
+}
